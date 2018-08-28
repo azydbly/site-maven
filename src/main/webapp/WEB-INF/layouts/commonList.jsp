e0065c6e8b97976b9fcf5ae39967bb1a3041730b
yanpei9898 <614749555@qq.com>
2018-08-28 16:45:58 +0800

    1.0

---
+++ src/main/webapp/WEB-INF/layouts/commonList.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<link rel="stylesheet" type="text/css" href="${ctxstc}/style/css/util/datables_style.css" />
+
+<script src="${ctxstc}/styles/datatables/1.10.0/jquery.dataTables.min.js"></script>
+<script src="${ctxstc}/styles/datatables/datatables.optimize.js"></script>
+
+<script src="${ctxstc}/style/js/util/datables_style.js"></script>
---
+++ src/main/webapp/static/style/js/util/datables_style.js
@@ -0,0 +1,29 @@
+/* List 页面通过部分 */
+clazz = new Array('danger','success');
+text = new Array('已禁用','已启用');
+icon = new Array('&#xe615;','&#xe631;');
+state = new Array('启用','停用');
+flag = new Array('1','0');
+sex = new Array('男','女');
+
+/* 角色管理 List 页面  */
+Type = new Array('default','warning','secondary','primary');
+roletype = new Array('未定义','管理','技术','实施');
+
+/* 薪资管理 List */
+checkstate = new Array('default','primary','warning','success','danger','warning','warning');
+checkstateName = new Array('未定义','起草','审核中','审核通过','驳回','变更','变更审核中');
+
+/* 招聘管理中简历管理  */
+resume = new Array('primary','secondary','secondary','secondary','warning','success','danger');
+resumename = new Array('存档','推荐面试','推荐二面','推荐三面','建议录用','录用','驳回');
+resumeicon = new Array('&#xe615;','','','','','','');
+resumestate = new Array('推荐面试','','','','','','');
+
+/* 档案管理 */
+psnfile = new Array('','primary','success','danger','success');
+psnfilename = new Array('','实习','正式员工','离职','实习员工');
+
+/* 培训计划 */
+plan = new Array('secondary','success','warning','danger');
+planname = new Array('有待反馈','优','良','差');
