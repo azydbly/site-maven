16a4c0ef1b0dd518aee3d1bcf2770040592f4418
yanpei9898 <614749555@qq.com>
2018-09-17 10:27:59 +0800

    1.0

---
+++ src/main/java/com/xst/mapper/RoleMapper.java
@@ -0,0 +1,19 @@
+package com.xst.mapper;
+
+import com.xst.model.Roles;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @ClassName: RoleMapper
+ * @类描述： RoleMapper 角色mapper
+ * @Author: xiaoshitou
+ * @Email: 15324399524@163.com
+ * @CreateDate： 2018/9/17 10:26
+ */
+public interface RoleMapper {
+
+    //菜单分页显示
+    List<Roles> getPageRoleList(@Param("search") String search, @Param("subSQL") String subSQL);
+}
